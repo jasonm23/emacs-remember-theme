bd7a7eeeabbfc24ea9d9c7c214c5ed85fb1b71a8
Jasonm23 <jasonm23@gmail.com>
2013-07-18 12:34:40 +1000

    Version 20130718.230

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
 # Emacs - Remember Theme
 
+#### Version: 20130718.230
+
+## Commentary
+
 I keep my `.emacs` in source control, and use the same defaults on all
 the machines I use. However, I like to have different themes on
 different machines.
 
 To help me do this automatically, I've created this little feature that
 remembers the current theme when Emacs closes, and loads it again when
-you start up.
+you start up (clearing any other loaded themes first.)
 
-If you install via elpa (marmalade repo) everything is set up for you
-automatically.
+## Installation
 
-Just install with: `M-x package-install remember-theme`
+Install from the marmalade repo via elpa/pacakge.el, and everything
+is set up for you automatically.
+
+Install with: `M-x package-install remember-theme`
+
+### Changelog
+
+* 20130718.230
+ * Updated documentation/header
+* 20130716.1310
+ * Fixed typos
+* 20130716.311
+ * Unload all loaded themes before loading the remembered theme.
+* 0.1.2 
+ * Fix bug for non existent .emacs-theme
+* 0.1.1
+ * Fix autoloads
+* 0.1.0
+ * Init
+
--- remember-theme.el
+++ remember-theme.el
@@ -1,54 +1,58 @@
 ;;; remember-theme.el --- Remembers the last theme in use and re-loads for the next session.
-;;;
-;;; Author: Jason Milkins <jasonm23@gmail.com>
-;;; Url: https://github.com/jasonm23/emacs-remember-theme
-;;; Version: 20130716.1310
-;;;
-;;; Changelog :
-;;;
-;;; 20130716.1310 L Fixed typo
-;;;
-;;; 20130716.311 : Unload all loaded themes before loading the
-;;; remembered theme.
-;;;
-;;;        0.1.2 : Fix bug for non existent .emacs-theme
-;;;
-;;;        0.1.1 : Fix autoloads
-;;;
-;;;        0.1.0 : Init
-;;;
-;;; This program is free software: you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation, version 3 of the License.
-;;;
-;;; This file is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with GNU Emacs.
-;;;
-;;; This file is not a part of Emacs
-;;;
-;;; Commentary:
-;;;
-;;; Emacs Remember Theme
-;;;  
-;;; I keep my `.emacs` in source control, and use the same defaults on all
-;;; the machines I use. However, I like to have different themes on
-;;; different machines.
-;;; 
-;;; To help me do this automatically, I've created this little feature that
-;;; remembers the current theme when Emacs closes, and loads it again when
-;;; you start up (clearing any other loaded themes first.)
-;;; 
-;;; If you install via elpa (marmalade repo) everything is set up for you
-;;; automatically.
-;;; 
-;;; Just install with: `M-x package-install remember-theme`
-;;;
 
+;; Author: Jason Milkins <jasonm23@gmail.com>
+;; Url: https://github.com/jasonm23/emacs-remember-theme
+;; Version: 20130718.230
+
+;;; Commentary:
+
+;; I keep my `.emacs` in source control, and use the same defaults on all
+;; the machines I use. However, I like to have different themes on
+;; different machines.
+;; 
+;; To help me do this automatically, I've created this little feature that
+;; remembers the current theme when Emacs closes, and loads it again when
+;; you start up (clearing any other loaded themes first.)
+
+;;; Installation:
+
+;; Install from the marmalade repo via elpa/pacakge.el, and everything
+;; is set up for you automatically.
+;; 
+;; Install with: `M-x package-install remember-theme`
+
+;; Changelog:
+;; 20130718.230
+;; * Updated documentation/header
+;; 20130716.1310
+;;  * Fixed typos
+;; 20130716.311
+;;  * Unload all loaded themes before loading the remembered theme.
+;; 0.1.2 
+;;  * Fix bug for non existent .emacs-theme
+;; 0.1.1
+;;  * Fix autoloads
+;; 0.1.0
+;;  * Init
+;;
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, version 3 of the License.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.
+;;
+;; This file is not a part of Emacs
+
+;;; Code:
 
 ;;;###autoload
 (defun remember-theme-save ()
