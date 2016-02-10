771227d0c7f4299c3db7d46812b9b9d01b8edc8b
Jason Milkins <jasonm23@gmail.com>
2016-02-10 16:55:12 +0800

    Rename to remember-themes

--- remember-themes.el
+++ remember-themes.el
@@ -1,13 +1,13 @@
-;;; remember-theme.el --- Remembers the last theme in use and re-loads for the next session.
+;;; remember-themes.el --- Remembers the last theme in use and re-loads for the next session.
 
 ;; Author: Jason Milkins <jasonm23@gmail.com>
 ;; Url: https://github.com/jasonm23/emacs-remember-theme
-;; Version: 20150308.1931
+;; Version: 20160210.1644
 
 ;;; Commentary:
 
 ;; I keep my `.emacs` in source control, and use the same defaults on all
-;; the machines I use. However, I like to have different themes on
+;; the machines I use. However, I do like to have different themes on
 ;; different machines.
 ;;
 ;; To help me do this automatically, I've created this little feature that
@@ -29,6 +29,8 @@
 ;; Install with: `M-x package-install remember-theme`
 
 ;;; Changelog:
+;; 20160210.1644
+;; * Rename to avoid inclusion in custom-available-themes
 ;; 20150308.1931
 ;; * Stop forcing load at after-init, update instructions.
 ;; 20140122.1500
@@ -118,10 +120,10 @@ If no `remember-theme-emacs-dot-file' file exists the operation is skipped."
       (let* ((theme-name (remember-theme-read))
              (theme-symbol (intern theme-name)))
         (unless (member theme-symbol custom-enabled-themes)
-          (require (intern (format "%s-theme" theme-name))))       
+          (require (intern (format "%s-theme" theme-name))))
         (load-theme theme-symbol))
     (run-hooks 'remember-theme-after-load-hook)))
 
-(provide 'remember-theme)
+(provide 'remember-themes)
 
-;;; remember-theme.el ends here
+;;; remember-themes.el ends here
